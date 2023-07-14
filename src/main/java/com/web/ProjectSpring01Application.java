50105e5227b3a6a38ba13b396920742294024fd1
LeoPCX <leopcxw@gmail.com>
2023-07-14 16:11:23 -0300

    Project Created

---
+++ src/main/java/com/web/ProjectSpring01Application.java
@@ -0,0 +1,12 @@
+  package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpring01Application.class, args);
+	}
+}
---
+++ src/test/java/com/web/ProjectSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
